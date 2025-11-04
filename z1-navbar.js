2e0c70bf8f8198cd7f3f62b5dab52ce2ab7fb55d
VikiStringz <107577144+ProgViki@users.noreply.github.com>
2025-11-04 23:55:06 +0100

    Update z1-navbar.js

--- z1-navbar.js
+++ z1-navbar.js
@@ -1,1 +1,9 @@
+document.getElementById("startBtn").addEventListener("click", () => {
+  const email = document.querySelector("input[type='email']").value;
+  if (email.trim() === "") {
+    alert("Please enter your email address");
+  } else {
+    alert(`Welcome, ${email}! We'll reach out to you soon.`);
+  }
+});
 
