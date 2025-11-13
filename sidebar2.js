d83a13d66719315aba17fdeb987745f4af994909
VikiStringz <107577144+ProgViki@users.noreply.github.com>
2025-11-14 00:25:18 +0100

    Update sidebar2.js

--- sidebar2.js
+++ sidebar2.js
@@ -1,9 +1,33 @@
-// codes
-document.getElementById("startBtn").addEventListener("click", () => {
-  const email = document.querySelector("input[type='email']").value;
-  if (email.trim() === "") {
-    alert("Please enter your email address");
-  } else {
-    alert(`Welcome, ${email}! We'll reach out to you soon.`);
-  }
+const expand_btn = document.querySelector(".expand-btn");
+
+let activeIndex;
+
+expand_btn.addEventListener("click", () => {
+    document.body.classList.toggle("collapsed");
+});
+
+const current = window.location.href;
+const allLinks = document.querySelectorAll(".sidebar-links a");
+
+allLinks.forEach((elem) => {
+    elem.addEventListener("click", () => {
+        const hrefLinkClick = elem.href;
+
+        allLinks.forEach((linkElem, index) => {
+            if (linkElem.href === hrefLinkClick) {
+                activeIndex = index;
+                console.log("activeIndex", activeIndex);
+                setActiveLink();
+            } else {
+                linkElem.classList.remove("active"); // remove active class from all links
+
+            }
+        });
+    });
+});
+
+const searchInput = document.getElementById("search-input");
+
+searchInput.addEventListener("input", () => {
+    document.body.classList.remove("collapsed"); // remove collapsed class when search input is focused
 });
